8b048861bc26c7306d1c6222d0d40593b5e03164
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 22:43:04 -0500

    Update P4_Vertical.java

--- src/P4_Vertical.java
+++ src/P4_Vertical.java
@@ -1,12 +1,29 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/1/2018
 */
 
 public class P4_Vertical {
 
 	public static void main(String[] args) {
+		Vertical("hey now");
+	}
+	
+	
+	
+	
+
+	public static void Vertical(String str){
+
+	    if(str != null && !"".equals(str)){
+	        int sLen = str.length();
+
+	        for (int i=0; i<sLen; i++){
+	            System.out.println(str.charAt(i));
+	        }
+
+	    }
 
 	}
 
