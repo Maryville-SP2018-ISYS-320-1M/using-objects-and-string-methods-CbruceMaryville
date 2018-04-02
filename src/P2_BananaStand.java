c9e0d200c100abaaae67231dcdfe1b165eed2f66
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 22:38:08 -0500

    Update P2_BananaStand.java

--- src/P2_BananaStand.java
+++ src/P2_BananaStand.java
@@ -1,13 +1,32 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/1/2018
 */
-
 
 public class P2_BananaStand {
 	public static void main(String[] args) {
-		String quote = "There's always money in the banana stand.";
-		
+		LastHalf("There's always money in the banana stand.");
+
 	}
+
+	
+	
+	
+	
+	
+	public static String LastHalf(String S) {
+			//String quote = "There's always money in the banana stand.";
+			int len;
+			len = S.length();
+			int len2;
+			len2 = (len/2);
+			
+		//	System.out.println(S.substring(len2,41));
+	// the system print works but why wont the return statement
+		
+			
+			return (S.substring(len2,41));
+	}
+
 }
